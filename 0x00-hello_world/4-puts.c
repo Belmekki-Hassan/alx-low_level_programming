1d0ff80e7b66919d0e9dd6a87cfb4861ccb537ae
Hassan Belmekki <hassanbelmekki@gmail.com>
2023-03-16 10:46:49 -0700

    the puts program and printf program are updated and betty approved

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 
 /**
-
+ *Adding main function as int and return 0 as successful
  */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
