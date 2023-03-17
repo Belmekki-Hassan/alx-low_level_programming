99ebbe4840ab3d7e1e4cc45d9231e2694ed4a1a4
Hassan Belmekki <hassanbelmekki@gmail.com>
2023-03-17 16:39:42 -0700

    Finally fixing the unwanted space and adding the comment

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 /**
- * main - that where all the fun begin
+ * main - positive; zero or negative
+*
  *Return: 0 (Success)
  */
 int main(void)
 {
-int n = rand();
+
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
 if (n > 0)
 {
-printf("%d is positive\n",n);
+printf("%d is positive\n", n);
 }
-else if (n == 0)
+else if (n < 0)
 {
-printf("%d is zero\n",n);
+printf("%d is neagative\n", n);
 }
 else
 {
-printf("%d is negative",n);
+printf("%d is zero", n);
 }
+
 return (0);
+
 }
