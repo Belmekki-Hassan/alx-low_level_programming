6dfd1214975d6676b80547eab09ea428bc6185c1
Hassan Belmekki <hassanbelmekki@gmail.com>
2023-03-17 16:54:21 -0700

    the last digit and alphabet printer program added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this is the forced comment i need to add
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+char alphabet = 'a';
+while (alphabet != '{')
+{
+putchar(alphabet);
+alphabet++;
+}
+putchar('\n');
+return (0);
+}
