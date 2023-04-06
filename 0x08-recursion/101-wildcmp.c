f9c3c315355beda79c798e169f7cda242212e300
Hassan Belmekki <hassanbelmekki@gmail.com>
2023-04-05 19:02:16 -0700

    wildcmp program updated

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,6 +3,7 @@
  * wildcmp - Compare strings
  * @s1: pointer to string params
  * @s2: pointer to string params
+ * Return: 0
  */
 int wildcmp(char *s1, char *s2)
 {
@@ -14,13 +15,13 @@ return (wildcmp(s1, s2 + 1));
 }
 return (*s2 == '\0');
 }
-if (*s2 == '*')
+else if (*s2 == '*')
 {
 return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 }
 else if (*s1 == *s2)
 {
-return (wildcmp(s1 + 1, s2 + 1)):
+return (wildcmp(s1 + 1, s2 + 1));
 }
 return (0);
 }
