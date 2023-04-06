aecd7f58697bbf5258bfeeab493ea48565841070
Hassan Belmekki <hassanbelmekki@gmail.com>
2023-04-05 18:10:38 -0700

    testing if number is prime or not

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,15 @@
+#incude "main.h"
+/**
+ * is_prime_number - evaulate a number
+ * @n : the number
+ * Return 1 is primer, otherwise 0
+ */
+int is_prime_number(int n)
+{
+if (n < 0)
+return (0);
+if ((n % 2 != 0) && (n / 3 != 0) && (n % 5 != 0))
+return (1);
+else
+return (0);
+}
