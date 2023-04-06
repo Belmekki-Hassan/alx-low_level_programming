814c036ceed3c024be81d24f211096201887889b
Hassan Belmekki <hassanbelmekki@gmail.com>
2023-04-05 18:15:53 -0700

    Inception updated

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,9 +7,15 @@
 int is_prime_number(int n)
 {
 if (n < 0)
+{
 return (0);
-if ((n % 2 != 0) && (n / 3 != 0) && (n % 5 != 0))
+}
+else if ((n % 2 != 0) && (n / 3 != 0) && (n % 5 != 0))
+{
 return (1);
+}
 else
+{
 return (0);
+}
 }
