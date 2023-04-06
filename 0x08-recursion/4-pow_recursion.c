cc177da867812a91cb1ebdb7b08788df1eac21c0
Hassan Belmekki <hassanbelmekki@gmail.com>
2023-04-05 17:54:13 -0700

    pow of an integer recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - power of an int
+ * @x: the number
+ * @y: the power
+ * Return: -1 if y is less than 0
+ */
+int _pow_recursion(int x, int y)
+{
+if (y == 0)
+return (1);
+else if (y < 0)
+return (-1);
+else
+return (x * _pow_recursion(x, y - 1));
+}
