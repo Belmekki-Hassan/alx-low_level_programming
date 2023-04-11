0447109ef471fb6c22b8d5c14ddbfe32d7b86932
Hassan Belmekki <hassanbelmekki@gmail.com>
2023-04-10 18:39:19 -0700

    Display number of args passed into main

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Display the number of arguments in the main
+ * @argc: Nbr of cli arg
+ * @argv: List name
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int i;
+if (argc == 1)
+{
+printf("%d\n", argc - 1)
+}
+for (i = 0; *argv; i++)
+{
+argv++;
+}
+printf("%d\n", i - 1);
+return (0);
+}
