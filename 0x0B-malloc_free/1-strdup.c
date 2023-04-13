97fbb6d6eb680712f69b7ced81c0667b2ce4c080
Hassan Belmekki <hassanbelmekki@gmail.com>
2023-04-12 17:42:38 -0700

    Add the functions

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* _strdup - Entry point
+* @str: Duplicated String
+* Return: if success display duplicated string else display null
+*/
+
+char *_strdup (char *str)
+{
+char *strnv = NULL;
+unsigned int i;
+int n;
+if (str == NULL)
+return (NULL);
+for (n = 0; str[n] != '\0'; n++)
+{
+}
+strnv = (char *)malloc(n + 1 * sizeof(char));
+if (strnv != NULL)
+{
+for (i = 0; str[i] != '\0'; i++)
+strnv[i] = str[i];
+}
+else
+{
+return (NULL);
+}
+strnv[i] = '\0';
+return (strnv);
+}
