7cc03748c404a294534eaceca707e0881aa48c6e
Hassan Belmekki <hassanbelmekki@gmail.com>
2023-04-14 18:04:40 -0700

    Static library updated

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -6,7 +6,7 @@
  * @n: the number of bytes
  * Return: copied memory with it byted changed
  */
-char *_memcpy(char *dest, char *src, unsinged int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 int c;
 int i = n;
