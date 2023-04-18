4c85fcef09f74de5495b7e038862cd7bc6f0a662
Hassan Belmekki <hassanbelmekki@gmail.com>
2023-04-17 17:51:36 -0700

    Preprocessor updated

---
+++ 0x0D-preprocessor/1-main.c
@@ -0,0 +1,16 @@
+#include "1-pi.h"
+#include "1-pi.h"
+#include <stdio.h>
+/**
+ * main - this is the main code
+ * Return: 0
+ */
+int main(void)
+{
+float f1;
+float f2;
+f2 = 98;
+f1 = PI * f2 * f2;
+printf("%.3f\n",f1);
+return (0);
+}
