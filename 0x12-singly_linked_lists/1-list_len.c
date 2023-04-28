b7049b56bab0fa5fdc008ef9bef7efcccd3b1104
Hassan Belmekki <hassanbelmekki@gmail.com>
2023-04-27 17:22:02 -0700

    Alx 12 singly linked lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * list_len - check the code
+ * @h: a
+ * Return: Always 0.
+ */
+size_t list_len(const list_t *h)
+{
+const list_t *nav = h;
+size_t count = 0;
+while (nav)
+{
+count++;
+nav = nav->next;
+}
+return (count);
+}
---
+++ 0x12-singly_linked_lists/main.h
@@ -0,0 +1,21 @@
+#ifndef main_h
+#define main_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * struct list_s - singly linked list
+ * @str: string variable (malloc string)
+ * @len: length of a string
+ * @next: points to the next node
+ * Description: singly linked list node struct
+ */
+typedef struct list_s
+{
+char *str;
+unsigned int len;
+struct list_s *next;
+} list_t;
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+#endif
