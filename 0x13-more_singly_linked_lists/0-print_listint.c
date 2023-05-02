3f562483ec919728443fe2f4a3bed757f66b419c
Hassan Belmekki <hassanbelmekki@gmail.com>
2023-05-01 17:57:05 -0700

    Print count listint Updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 /**
- * print_listint - function that count the elements of a list
+ * print_listint - function that prints all the elements of a listint_t list
  * @h: pointer to the node head
  * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-const listint_t *head = h;
+const listint_t *nav = h;
 size_t count = 0;
-while (head)
+while (nav)
 {
-printf("%d\n", head->n);
+printf("%d\n", nav->n);
 count++;
-head = head->next;
+nav = nav->next;
 }
 return (count);
 }
