7a9d3d6c1ba4910ef6caac9ce40519d51253892f
Hassan Belmekki <hassanbelmekki@gmail.com>
2023-05-05 18:16:02 -0700

    Task 0 added

---
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * binary_to_unit - function that convert binary to unit
+ * @b: binary variable
+ * Return: 0
+ */
+unsigned int binary_to_unit(const char *b)
+{
+unsigned int unit = 0;
+int i = 0, length = 0;
+if (!b)
+return (0);
+while (b[lenth + 1])
+{
+if (b[length] != '0' && b[length] != '1')
+return (0);
+length++;
+}
+for (i = 0; i<= length; i++)
+{
+if (b[i] == '1')
+unit = unit + (1 << (lenth - 1));
+}
+return (unit);
+}
